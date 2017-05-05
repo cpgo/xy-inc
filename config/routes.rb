65538307a444fd8b3a8266f0f00dd0f59d839667
Cassio Godinho <cassiopgodinho@gmail.com>
2017-05-05 16:55:06 -0300

    add cors package

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: "pois#index"
-  get "/near", to: "proximity_list#index"
-  resources "pois"
+  root to: "pois#index", :defaults => { :format => 'json' }
+  get "/near", to: "proximity_list#index", :defaults => { :format => 'json' }
+  resources "pois", :defaults => { :format => 'json' }
 end

0d854ed85944316354378d6bf81b915b4e0dcdb0
Cassio Godinho <cassiopgodinho@gmail.com>
2017-05-08 16:40:26 -0300

    fix route name on tests

--- test/controllers/pois_controller_test.rb
+++ test/controllers/pois_controller_test.rb
@@ -2,14 +2,14 @@ require 'test_helper'
 
 class PoisControllerTest < ActionDispatch::IntegrationTest
   test "index lists all pois" do
-    get "/pois"
+    get "/poi"
     assert(JSON.parse(@response.body).count, 6)
     assert_equal(@response.content_type, "application/json")
   end
 
   test "create with invalid params renders errors" do
     invalid_params = {poi: {y: -1}}
-    post "/pois", params: invalid_params
+    post "/poi", params: invalid_params
     errors = {
               name: ["can't be blank"],
               x: ["can't be blank", "is not a number"],
@@ -20,7 +20,7 @@ class PoisControllerTest < ActionDispatch::IntegrationTest
 
   test "create with valid params renders the poi" do
     valid_params = {poi: {x: 10, y: 15, name: "POI name"}}
-    post "/pois", params: valid_params
+    post "/poi", params: valid_params
     json_response = JSON.parse(@response.body)
     assert_equal(json_response["x"], valid_params[:poi][:x])
     assert_equal(json_response["y"], valid_params[:poi][:y])

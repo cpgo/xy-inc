15a4221e45a8585823871d82879bb8f228940cc3
Cassio Godinho <cassio.godinho@zup.com.br>
2017-08-28 14:50:19 -0300

    adasd

--- app/models/poi.rb
+++ app/models/poi.rb
@@ -2,9 +2,7 @@ class Poi < ApplicationRecord
   validates :name, presence: true
   validates :x, presence: true, numericality: { only_integer: true , greater_than: 0}
   validates :y, presence: true, numericality: { only_integer: true , greater_than: 0}
-
-  def coords
+    def coords
     {x: x, y: y}
   end
-  
 end

98511451fcc6ac1aa9379a97d234a50222ba679e
Cassio Godinho <cassio.godinho@zup.com.br>
2017-08-28 15:16:20 -0300

    two new

--- app/models/poi.rb
+++ app/models/poi.rb
@@ -3,6 +3,14 @@ class Poi < ApplicationRecord
   validates :x, presence: true, numericality: { only_integer: true , greater_than: 0}
   validates :y, presence: true, numericality: { only_integer: true , greater_than: 0}
 
+  def test1
+    "test"
+  end
+
+  def test2
+    "test2"
+  end
+
   def coords
     {x: x, y: y}
   end

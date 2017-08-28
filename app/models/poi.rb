acc4ccdf2ca67c6c6c7e433ee719461c76df0b44
Cassio Godinho <cassio.godinho@zup.com.br>
2017-08-28 14:30:15 -0300

    bad

--- app/models/poi.rb
+++ app/models/poi.rb
@@ -2,7 +2,8 @@ class Poi < ApplicationRecord
   validates :name, presence: true
   validates :x, presence: true, numericality: { only_integer: true , greater_than: 0}
   validates :y, presence: true, numericality: { only_integer: true , greater_than: 0}
-
+  STUFF = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"
+  # asdhfiaushdfiuahsfi ashifd uahifu haiufh iuash fiuha siuhf iuash fiuh asdudhiasiduf hias hdsh iuh iduais dfiahiduf hihd iifdasfs
   def coords
     {x: x, y: y}
   end
